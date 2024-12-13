94eb655b533916ef4d5dcef66c690f2c130efe4c
AdhamHaithamEid <adhamhaithameid@gmail.com>
2024-12-13 19:13:05 +0200

    made the ui of the info page

---
+++ docs/info/info.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const collapsibleButtons = document.querySelectorAll(".collapsible-btn");
+
+  collapsibleButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const targetSelector = btn.getAttribute("data-target");
+      const target = document.querySelector(targetSelector);
+      if (target) {
+        target.classList.toggle("hidden");
+      }
+    });
+  });
+});
