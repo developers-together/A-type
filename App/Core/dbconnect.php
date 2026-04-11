9984b4518c1166af851d8c99a10fc50da5c0b244
AdhamHaithamEid <adhamhaithameid@gmail.com>
2026-04-11 15:41:42 +0200

    chore: bootstrap laravel app and remove custom mvc scaffold

--- App/Core/dbconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define("DB", "mysql");
-define("DB_NAME", getenv('DB_NAME'));
-define("DB_USER", getenv('DB_USER'));
-define("DB_PASS", getenv('DB_PASSWORD'));
-define("DB_URL", getenv('DB_HOST'));
--- Public/js/modules/utils.js
+++
@@ -1,7 +0,0 @@
-export function formatWord(word) {
-  return `<div class="word">
-    <span class="letter">${word
-      .split("")
-      .join('</span><span class="letter">')}</span>
-    </div>`;
-}
